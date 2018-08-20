8eeb1da5296c176974229da1b072e225a56f9305
V0173 <42351026+V0173@users.noreply.github.com>
2018-08-20 10:53:31 +0300

    Update save.php

--- roguelike/save.php
+++ roguelike/save.php
@@ -1,11 +1,11 @@
 <?php
 
-$loc="save.txt";
-$str="hello world!";
+$loc = "save.txt";
+$str = "hello world!";
 
 file_put_contents($loc,$str);
 
-$str=file_get_contents($loc);
+$str = file_get_contents($loc);
 
 echo $str;
 
